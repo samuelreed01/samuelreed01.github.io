d790e170e5b3c60a0b1b312af040237679f18ac2
Samuel Reed <samuelreed01@gmail.com>
2026-09-02 12:15:40 +0700

    update node versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rtp314.github.io',
+  site: "https://samuelreed01.github.io",
 });
